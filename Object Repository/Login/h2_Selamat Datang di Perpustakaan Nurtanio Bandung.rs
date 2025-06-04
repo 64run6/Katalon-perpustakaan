a26eac8acaae4fa7c98d1f2fe51307180ba99f79
Modern <Modern@G4rung>
2025-06-04 13:56:10 +0700

    o

---
+++ Object Repository/Login/h2_Selamat Datang di Perpustakaan Nurtanio Bandung.rs
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h2_Selamat Datang di Perpustakaan Nurtanio Bandung</name>
+   <tag></tag>
+   <elementGuidId>4e6d0851-89e4-4658-9d0a-243f0fd0702f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='app']/div[2]/div/h2</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h2</value>
+      <webElementGuid>706c0ca6-c14a-483d-9e5b-ac8ac6203a93</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>text-3xl md:text-4xl font-extrabold text-gray-800 mb-3 heading-shadow tracking-wide welcome-title</value>
+      <webElementGuid>8084c1c4-2392-40dc-9ffa-ad6f1c214ad1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value> 📖 Selamat Datang di Perpustakaan Nurtanio Bandung 📖 </value>
+      <webElementGuid>c95a3e88-1928-4810-ab10-c03fe3d6500f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;app&quot;)/div[@class=&quot;welcome-section&quot;]/div[@class=&quot;max-w-6xl mx-auto py-8 px-4 text-center fade-in&quot;]/h2[@class=&quot;text-3xl md:text-4xl font-extrabold text-gray-800 mb-3 heading-shadow tracking-wide welcome-title&quot;]</value>
+      <webElementGuid>b65c90a7-0dae-4cee-8332-f5c399453148</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='app']/div[2]/div/h2</value>
+      <webElementGuid>1c5a286f-43a1-41b6-9d1b-cd64823d1804</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='E-Book'])[2]/following::h2[1]</value>
+      <webElementGuid>70f7e5d9-3913-452b-a559-9ea2a4df0ffc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Buku Baru'])[2]/following::h2[1]</value>
+      <webElementGuid>afd5c66c-8aad-4214-b5e8-7fa1d7939a26</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Layanan Kami'])[1]/preceding::h2[1]</value>
+      <webElementGuid>cea47e90-7382-480d-a2b1-3d3257d31975</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Koleksi Perpustakaan'])[1]/preceding::h2[1]</value>
+      <webElementGuid>f3eac756-6f78-4d25-aa6a-5148135b36fd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='📖 Selamat Datang di Perpustakaan Nurtanio Bandung 📖']/parent::*</value>
+      <webElementGuid>c53c785d-f18b-4dcf-a852-49f075417346</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h2</value>
+      <webElementGuid>3a50a7c4-91bb-4842-a9a2-a2265a4629e9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h2[(text() = ' 📖 Selamat Datang di Perpustakaan Nurtanio Bandung 📖 ' or . = ' 📖 Selamat Datang di Perpustakaan Nurtanio Bandung 📖 ')]</value>
+      <webElementGuid>3512f721-1276-478b-89d6-25925e6ed8f8</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
